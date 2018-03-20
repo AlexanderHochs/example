d0f677a1acd18a88209bdfaa9e0666de3f8ba9dd
AlexandeHochs <alexanderhochschild@hotmail.com>
2018-03-20 13:17:19 -0500

    models with swat-pistol anims

---
+++ UnityDemo/weaponDemo/Assets/shootPistol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootPistol : MonoBehaviour {
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        Debug.Log(anim.enabled);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetTrigger("shoot");
+            Debug.Log("shoot");
+        }
+       // else if(Input.GetKeyDown(KeyCode.Tab))
+
+	}
+}
